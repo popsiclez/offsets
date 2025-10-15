d44b250f677fc92999b1bc93f89f99a30c31532c
popsiclez <dbartek9876@gmail.com>
2025-10-14 22:03:20 -0400

    Update output folder

---
+++ output/panorama_dll.hpp
@@ -0,0 +1,20 @@
+// Generated using https://github.com/a2x/cs2-dumper
+// 2025-10-15 02:03:18.791314200 UTC
+
+#pragma once
+
+#include <cstddef>
+
+namespace cs2_dumper {
+    namespace schemas {
+        // Module: panorama.dll
+        // Class count: 1
+        // Enum count: 0
+        namespace panorama_dll {
+            // Parent: None
+            // Field count: 0
+            namespace ELayoutNodeType {
+            }
+        }
+    }
+}
