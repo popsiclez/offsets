1d032e909137ae071f94aa975d81e4268ae27a5d
popsiclez <74466666+popsiclez@users.noreply.github.com>
2025-10-16 17:43:00 -0400

    Add files via upload

---
+++ output/panorama_dll.hpp
@@ -0,0 +1,20 @@
+// Generated using https://github.com/a2x/cs2-dumper
+// 2025-10-16 21:42:06.614757100 UTC
+
+#pragma once
+
+#include <cstddef>
+
+namespace cs2_dumper {
+    namespace schemas {
+        // Module: panorama.dll
+        // Class count: 1
+        // Enum count: 0
+        namespace panorama_dll {
+            // Parent: None
+            // Field count: 0
+            namespace ELayoutNodeType {
+            }
+        }
+    }
+}
